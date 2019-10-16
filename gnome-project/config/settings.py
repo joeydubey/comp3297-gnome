cd52524e9bff1dd603ebb0119acee4a7720030c9
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-10-16 11:13:17 +0800

    added backtrack to the config file to make new projects with constructor

--- gnome-project/config/settings.py
+++ gnome-project/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backtrack.apps.BackTrackConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
