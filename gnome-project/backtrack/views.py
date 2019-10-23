0c70e14a32e1c8aff2481f702c5bb28cb2700f09
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-10-23 17:04:08 +0800

    working sorta

--- gnome-project/backtrack/views.py
+++ gnome-project/backtrack/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from backtrack.models import Project, ProductBacklog, SprintBacklog
+import logging
 
 # Create your views here.
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class ViewProjects(TemplateView):
@@ -18,14 +21,16 @@ class ViewProjects(TemplateView):
 class ViewProject(TemplateView):
     template_name = "project.html"
 
-    # def get_context_data(self, **kwargs):
-    #     console.log(**kwargs)
-    #     project = self.kwargs['project']
-    #     context = super().get_context_data(**kwargs)
-    #     context['project'] = Project.objects.get(id=project)
-    #     return context
+    def get_context_data(self, **kwargs):
+        logging.debug(kwargs)
+        project = self.kwargs['project']
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.get(id=project)
+        return context
 
 
 class ProjectsViewAll(ListView):
     template_name = "project_list.html"
     model = Project
+
+    logging.debug('This will get logged')
