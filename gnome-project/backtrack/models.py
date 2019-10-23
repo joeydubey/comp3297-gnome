7a6695146ffff233a221725431adcd7404fc30e6
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-10-23 10:23:49 +0800

    made the models

--- gnome-project/backtrack/models.py
+++ gnome-project/backtrack/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductBacklog(models.Model):
+    name = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class SprintBacklog(ProductBacklog):
+
+    def __str__(self):
+        return self.name
+
+
+class ProductBacklogItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    pointEstimate = models.IntegerField()
+    productBacklog = models.ForeignKey(ProductBacklog)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    sprintBacklog = models.ForeignKey(SprintBacklog)
+    description = models.CharField(max_length=500)
+    estimatedEffortHours = models.TimeField()
+    actualEffortHours = models.TimeField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
