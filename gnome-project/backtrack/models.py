e4f53e89551e3c6eff05b5d092ee4614cbd21142
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-10-23 11:24:49 +0800

    made all urls

--- gnome-project/backtrack/models.py
+++ gnome-project/backtrack/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -26,7 +27,7 @@ class ProductBacklogItem(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     pointEstimate = models.IntegerField()
-    productBacklog = models.ForeignKey(ProductBacklog)
+    productBacklog = models.ForeignKey(ProductBacklog, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -34,7 +35,7 @@ class ProductBacklogItem(models.Model):
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    sprintBacklog = models.ForeignKey(SprintBacklog)
+    sprintBacklog = models.ForeignKey(SprintBacklog, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
     estimatedEffortHours = models.TimeField()
     actualEffortHours = models.TimeField()
--- gnome-project/backtrack/views.py
+++ gnome-project/backtrack/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from orders.models import Project, ProductBacklog, SprintBacklog
 
 # Create your views here.
+
+
+class ViewProjects(TemplateView):
+    template_name = "project_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.all()
+        return context
+
+
+class ViewProject(TemplateView):
+    template_name = "project.html"
+
+    def get_context_data(self, **kwargs):
+        project = self.kwargs['project']
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.all(pk = project)
+        return context
+
+
+class ProjectsViewAll(ListView):
+    template_name = "project_list.html"
+    model = Project
