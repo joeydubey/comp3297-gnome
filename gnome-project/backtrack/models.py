bd07fef41f5b6bc7131f775f2308f7e1f19c56c5
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-10-23 16:37:29 +0800

    fix db

--- gnome-project/backtrack/models.py
+++ gnome-project/backtrack/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, default='Not yet started')
 
     def __str__(self):
         return self.name
@@ -40,7 +40,7 @@ class Task(models.Model):
     estimatedEffortHours = models.TimeField()
     actualEffortHours = models.TimeField()
     status = models.CharField(max_length=50)
-    pbi = models.ForeignKey(ProductBacklogItem, on_delete=models.CASCADE)
+    pbi = models.ForeignKey(ProductBacklogItem, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
