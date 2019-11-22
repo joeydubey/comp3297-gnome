1c9d66835ad318b666885e9e980a942a775e2127
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-11-22 14:21:58 -0800

    fixed

--- gnome-project/config/urls.py
+++ gnome-project/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView # new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('backtrack/', include('backtrack.urls')),
+    path('backtrack/', include('backtrack.urls'), name='backtrack'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('backtrack/projects', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
