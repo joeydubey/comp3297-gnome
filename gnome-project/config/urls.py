0a3e37a89fe6531249a711805e835925f68b3ee0
Hannah Grossman <hannahgrossman@berkeley.edu>
2019-12-05 16:06:33 -0800

    real changes

--- gnome-project/config/urls.py
+++ gnome-project/config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('backtrack/', include('backtrack.urls'), name='backtrack'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('backtrack/projects', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
